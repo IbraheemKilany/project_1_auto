5548313998c3550e9bec1b0613ad9330d5f6c5b6
ibraheem <kilany970@gmail.com>
2023-09-30 22:58:30 +0300

    new update

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+describe('aspaire test case', () => {
+  it.skip('check the currency is sar', () => {
+    cy.visit('https://www.almosafer.com/en')
+    cy.get('.cta__saudi') .click()
+    cy.get('[data-testid="Header__CurrencySelector"]').invoke("text").should("include","SAR")
+  })
+  it('to check language', () => {
+    cy.visit('https://www.almosafer.com/en')
+    cy.get('.cta__saudi') .click()
+    cy.get('html').should('have.attr','lang','ar')
+  });
+})
